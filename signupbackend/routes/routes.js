1dfaa39141a25b74a30c5c457d7249f234688beb
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-23 10:48:40 -0500

    test server & db connection

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -6,10 +6,10 @@ const signUpTemplateCopy = require('../models/SignUpModels')
 
 router.post('/signup', (request, response) => {
   const signedUpUser = new signUpTemplateCopy({
-    fullName:request.body.fillName,
+    fullName:request.body.fullName,
     username:request.body.username,
     email:request.body.username,
-    password:request.body,password
+    password:request.body.password
   })
   signedUpUser.save()
   .then(data => {
--- signupbackend/test.js
+++
@@ -1,5 +0,0 @@
-const superagent = require('superagent');
-
-superagent
-.post('app/signup')
-.send({fullName: "Adam", username: "Ad", email: "adam@gmail.com", password: "adamadam"})
